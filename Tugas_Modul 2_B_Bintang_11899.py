8c8ba225a44534f2fd566a7ee39bd466ba626585
bintang-anindya <220711899@students.uajy.ac.id>
2024-09-25 20:10:25 +0700

    Update Tugas_Modul 2_B_Bintang_11899.py

--- Tugas_Modul 2_B_Bintang_11899.py
+++ Tugas_Modul 2_B_Bintang_11899.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = r'C:\Users\ROG\Documents\Pembelajaran Mesin\Model Preparation and Evaluation'
-model=os.path. join(model_path,'GBT_heartDisease_model.pkl')
+model_path = 'GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
